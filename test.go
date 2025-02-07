016d11447d054006fc97efb70ea90b42bec388a3
alexmanfait <alex.manfait@ynov.com>
2025-02-07 11:29:46 +0100

    zzz

--- test.go
+++ test.go
@@ -7,6 +7,6 @@ import(
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, <doc html à mettre là gros pd)
+		fmt.Fprintf(w, "<html><body>Géolocalisation réussie</body></html>")
 	}                                                                                                                                                                                                                                                                        )
 }
